4f8b049513134388f5bb3ba4a3803863e1f79301
Малофеев Антон Владимирович <9151532547@mail.ru>
2022-10-04 00:09:49 +0300

    Stage 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
-    private static final String message = "we found a treasure!";
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static String message, key;
     private static StringBuilder encryptedMessage = new StringBuilder();
 
     public static void main(String[] args) {
-        System.out.println(encryptionMethod());
+
+        Scanner sc = new Scanner(System.in);
+
+        message = sc.nextLine();
+        key = sc.nextLine();
+
+        System.out.println(encryptionMethod(message, key));
     }
 
-    static StringBuilder encryptionMethod(){
-        int min = (int) 'a';
-        int max = (int) 'z';
+    static StringBuilder encryptionMethod(String message, String key){
         char encryptedChar;
 
         for (char chr : message.toCharArray()) {
             if (Character.isAlphabetic(chr)) {
-                encryptedChar = (char) (max - (int) chr + min);
+                encryptedChar = ALPHABET.charAt((ALPHABET.indexOf(chr) + Integer.parseInt(key))%26);
                 encryptedMessage.append(encryptedChar);
             } else {
                 encryptedMessage.append(chr);
